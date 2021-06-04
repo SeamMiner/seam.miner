959b6405363237db2ba9561512c1e5a9917e2efa
SeamMiner <niatit130@gmail.com>
2021-06-04 22:59:28 +0300

    Add dist

---
+++ precache-manifest.b29e1e98d4af6503bf3ffed76ef42bf5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fefda29f3a1cb41646",
+    "url": "/seam.miner/css/app.98e1b410.css"
+  },
+  {
+    "revision": "08412639439510ebb7b148b44ce6778b",
+    "url": "/seam.miner/img/Telegram.08412639.png"
+  },
+  {
+    "revision": "83f314397b0d5e68e8a570ac174ba77b",
+    "url": "/seam.miner/index.html"
+  },
+  {
+    "revision": "6e01230c0002bd496245",
+    "url": "/seam.miner/js/404.0c87d480.js"
+  },
+  {
+    "revision": "25fefda29f3a1cb41646",
+    "url": "/seam.miner/js/app.60b1ddae.js"
+  },
+  {
+    "revision": "1fa9a037d428222d9d4a",
+    "url": "/seam.miner/js/chunk-vendors.c203f180.js"
+  },
+  {
+    "revision": "7c9a14b41d9a79ed7b8826a9890785d8",
+    "url": "/seam.miner/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/seam.miner/robots.txt"
+  }
+]);
