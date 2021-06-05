5d21f6dc6d5c824f13d1732dc0331af96ee5376a
SeamMiner <niatit130@gmail.com>
2021-06-05 19:10:01 +0300

    Release at 05.06.2021

---
+++ dist/precache-manifest.48d65529c31ad5acafdf830e19a02e51.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9c7c89652de6f67b60",
+    "url": "/SeamMiner/css/app.7e6d3d11.css"
+  },
+  {
+    "revision": "0f7b76ef0f36a1c59ff3819fbe1ab262",
+    "url": "/SeamMiner/img/Email.0f7b76ef.svg"
+  },
+  {
+    "revision": "54b4c9be604795250501b11e619e18ba",
+    "url": "/SeamMiner/img/GitHub.54b4c9be.svg"
+  },
+  {
+    "revision": "cb202f6bfa1463dc5d135f60112a9a42",
+    "url": "/SeamMiner/img/Telegram.cb202f6b.svg"
+  },
+  {
+    "revision": "7bd023e18221050aca2b165fb1fa81be",
+    "url": "/SeamMiner/index.html"
+  },
+  {
+    "revision": "9c99c94fcd92e842caa6",
+    "url": "/SeamMiner/js/404.fea4f6e0.js"
+  },
+  {
+    "revision": "df9c7c89652de6f67b60",
+    "url": "/SeamMiner/js/app.8cb0ca33.js"
+  },
+  {
+    "revision": "1636db0d266432d6f37d",
+    "url": "/SeamMiner/js/chunk-vendors.57d88e69.js"
+  },
+  {
+    "revision": "7c9a14b41d9a79ed7b8826a9890785d8",
+    "url": "/SeamMiner/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/SeamMiner/robots.txt"
+  }
+]);
